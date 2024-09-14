7b551d1deede2d5462629fffe4d9ce83669bd19a
Pedro Gregorio <pedro-gregorio@outlook.pt>
2024-09-14 17:58:31 +0100

    Add: Main State Logic

--- scripts/content.js
+++ scripts/content.js
@@ -1,24 +1,56 @@
-const elements = [
-  {
-    id: "shorts",
-    selector: "//a[@title='Shorts']/..",
-    hidden: false,
-  },
-  {
-    id: "tabs",
-    selector: "//ytd-feed-filter-chip-bar-renderer/..",
-    hidden: false,
-  },
-  {
-    id: "ads",
-    selector:
-      "//ytd-ad-slot-renderer/ancestor::ytd-rich-item-renderer | //*[@id='player-ads']",
-    hidden: false,
-  },
-];
+const setElementVisibilityOnce = (id, hidden) => {
+  let hasRun = false;
+  return () => {
+    if (!hasRun) {
+      setElementVisibility(id, hidden);
+      hasRun = true;
+    }
+  };
+};
 
-function hideElement(target, hide) {
+const waitForElements = (selectorToHide, callback) => {
+  const checkExist = setInterval(() => {
+    const elements = document.evaluate(
+      selectorToHide,
+      document,
+      null,
+      XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+      null
+    );
+
+    if (elements.snapshotLength > 0) {
+      clearInterval(checkExist);
+      callback(elements);
+    }
+  }, 200);
+};
+
+const getElements = () => {
+  return (
+    JSON.parse(localStorage.getItem("elements")) || [
+      {
+        id: "shorts",
+        selector: "//a[@title='Shorts']/..",
+        hidden: undefined,
+      },
+      {
+        id: "tabs",
+        selector: "//ytd-feed-filter-chip-bar-renderer/..",
+        hidden: undefined,
+      },
+      {
+        id: "ads",
+        selector:
+          "//ytd-ad-slot-renderer/ancestor::ytd-rich-item-renderer | //*[@id='player-ads']",
+        hidden: undefined,
+      },
+    ]
+  );
+};
+
+function setElementVisibility(target, hide) {
   let selectorToHide;
+  let elements = getElements();
 
   elements.forEach((element) => {
     if (element.id === target) {
@@ -38,13 +70,23 @@ function hideElement(target, hide) {
   for (let i = 0, length = elementsToHide.snapshotLength; i < length; i++) {
     elementsToHide.snapshotItem(i).style.display = hide ? "none" : "block";
   }
+  localStorage.setItem("elements", JSON.stringify(elements));
 }
 
 chrome.runtime.onMessage.addListener((request) => {
-  hideElement(request.action.target, request.action.hide);
-  chrome.runtime.sendMessage({ type: "info", data: elements });
+  setElementVisibility(request.action.target, request.action.hide);
+  chrome.runtime.sendMessage({ type: "info", data: getElements() });
 });
 
-// TODO: find a way to apply all the stored changes on page loading
-// TODO: If there is localStorage -> We use it and send information back to popup.js
-// TODO: Otherwise -> Nothing happens
+document.addEventListener("DOMContentLoaded", function () {
+  const elements = JSON.parse(localStorage.getItem("elements"));
+
+  if (elements) {
+    elements.forEach((element) => {
+      waitForElements(
+        element.selector,
+        setElementVisibilityOnce(element.id, element.hidden)
+      );
+    });
+  }
+});
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,17 +1,19 @@
 let elementsArray = document.querySelectorAll("input");
-let elements = [];
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log(elements);
-  console.log(elementsArray);
-
-  // TODO: Check elements initial state, wether if they're checked or not based on the storage
-
+  JSON.parse(localStorage.getItem("elements")) !== null &&
+    JSON.parse(localStorage.getItem("elements")).forEach((element) => {
+      document.getElementById(element.id).checked = element.hidden;
+    });
   elementsArray.forEach((element) => {
     element.addEventListener("change", () => {
       chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {
-          action: { target: element.id, hide: element.checked },
+          action: {
+            target: element.id,
+            hide: element.checked,
+            elements: JSON.parse(localStorage.getItem("elements")),
+          },
         });
       });
     });
@@ -20,8 +22,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 chrome.runtime.onMessage.addListener(function (message) {
   if (message.type === "popup") {
-    message.data.forEach((element) => {
-      elements.push(element);
-    });
+    localStorage.setItem("elements", JSON.stringify(message.data));
   }
 });
